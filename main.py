993b72ac590884ec856630473820d7fb175ab79d
GeeKaizer <georgemichaeldagogo@gmail.com>
2022-10-13 09:37:19 +0000

    new

--- main.py
+++ main.py
@@ -4,6 +4,9 @@ import pandas as pd
 import psycopg2
 from datetime import date
 import datetime 
+import psycopg2
+import pandas as pd
+from sqlalchemy import create_engine
 
 
 today = datetime.date.today()
@@ -29,9 +32,9 @@ def get_data():
     #keywords = ['Buhari','APC', 'PeterObi','Tinubu','Atiku']
     #it seems the api does not return every tweet containing at least one or every keyword, it returns the only tweets that contains every keyword
     #solution was to use the OR in the keywords string as this is for tweets search only and might give errors in pure python
-    limit = 10
+    limit = 100000
 
-    tweets = tweepy.Cursor(api.search_tweets, q = keywords,count = 200, tweet_mode = 'extended',geocode='9.0820,8.6753,450mi', until=week_ago).items(limit)
+    tweets = tweepy.Cursor(api.search_tweets, q = keywords,count = 200, tweet_mode = 'extended',geocode='9.0820,8.6753,450mi', until=yester).items(limit)
 
     columns = ['time_created', 'screen_name','name', 'tweet','loca_tion', 'descrip_tion','verified','followers', 'source','geo_enabled','retweet_count','truncated','lang','likes']
     data = []
@@ -45,58 +48,23 @@ def get_data():
     #removes retweeted tweets
     df = df.reset_index(drop = True)
 
-    df.to_csv('tweets.csv')
+    #conn_string = 'postgresql://user:password@host/database'
+    #conn_string = psycopg2.connect(database="test",user='postgres', password='george9042',host='localhost:5433')
+
+    conn_string = 'postgresql://postgres:george9042@localhost:5433/test'
     
-    sf = pd.read_csv('tweets.csv')
-    sf = sf.drop(sf.columns[0],axis = 1) #remove unnamed column sf[0]
-    
-    #sf.tweet = sf.tweet.str.replace(r'\W'," ")#replace all non aphabetic characters with space
-    #sf.descrip_tion = sf.descrip_tion.str.replace(r'\W'," ")
-    
+    db = create_engine(conn_string)
+    conn = db.connect()
 
-    sf.to_csv(r'tweets.csv', index = False, header=True) #save to same csv file
-
-
-def postgre_push():
-    conn = psycopg2.connect(database="test",
-                            user='postgres', password='george9042',
-                            host='127.0.0.1', port='5433'
-    )
-
+    df.to_sql('election', con=conn, if_exists='append',
+            index=False)
+    conn = psycopg2.connect(conn_string
+                            )
     conn.autocommit = True
     cursor = conn.cursor()
-
-
-    sql = ''' CREATE TABLE IF NOT EXISTS election_tweets
-            (    name varchar(100) NOT NULL,
-                time_created timestamp,
-                screen_name varchar(100),
-                tweet varchar(4000),
-                loca_tion varchar(200),
-                descrip_tion varchar,
-                verified varchar(10) NOT NULL,
-                followers int,
-                source varchar(50),
-                geo_enabled varchar(10) NOT NULL,
-                retweet_count int,
-                truncated varchar(10) NOT NULL,
-                lang varchar(10),
-                likes varchar(10) NOT NULL
-            ) 
-        '''
-
-
-    cursor.execute(sql)
-
-    sql2 = '''COPY election_tweets(time_created,screen_name,name,tweet,loca_tion,descrip_tion,verified,followers,source,geo_enabled,retweet_count,truncated,lang,likes)
-    FROM 'C:/Users/HP/Downloads/Primis/tweets.csv' DELIMITER ',' CSV HEADER;''' 
-
-    cursor.execute(sql2)
-
-   # sql3 = '''select * from election;'''
-    #cursor.execute(sql3)
-    #for i in cursor.fetchall():
-     #   print(i)
-
-    conn.commit()
+    
+    sql1 = '''DELETE FROM election WHERE time_created < current_timestamp - interval '7' day;'''
+    cursor.execute(sql1)
+    
+    # conn.commit()
     conn.close()
