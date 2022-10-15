b41260d09bbd4c82ed01e5a3ac207ef3a11a37aa
GeeKaizer <61968966+George-Michael-Dagogo@users.noreply.github.com>
2022-10-15 23:36:48 +0100

    new

--- main.py
+++ main.py
@@ -4,99 +4,70 @@ import pandas as pd
 import psycopg2
 from datetime import date
 import datetime 
-
-
-today = datetime.date.today()
-week_ago = datetime.date.today() - datetime.timedelta(days=7)
-yester = datetime.date.today() - datetime.timedelta(days=1)
+import psycopg2
+import pandas as pd
+from sqlalchemy import create_engine
 
 config = configparser.ConfigParser()
 config.read("config.ini")
 
+today = datetime.date.today()
+yester = datetime.date.today() - datetime.timedelta(days=1)
 
-def get_data():
-    api_key = config['twitter']['api_key']
-    api_key_secret = config['twitter']['api_key_secret']
-    access_token = config['twitter']['access_token']
-    access_token_secret = config['twitter']['access_token_secret']
+api_key = config['twitter']['api_key']
+api_key_secret = config['twitter']['api_key_secret']
+access_token = config['twitter']['access_token']
+access_token_secret = config['twitter']['access_token_secret']
 
-    auth = tweepy.OAuthHandler(api_key,api_key_secret)
-    auth.set_access_token(access_token,access_token_secret)
+auth = tweepy.OAuthHandler(api_key,api_key_secret)
+auth.set_access_token(access_token,access_token_secret)
 
-    api = tweepy.API(auth)
+api = tweepy.API(auth)
 
-    keywords = ['Buhari OR APC OR  PeterObi OR Tinubu OR PDP OR Atiku OR LabourParty']
-    #keywords = ['Buhari','APC', 'PeterObi','Tinubu','Atiku']
-    #it seems the api does not return every tweet containing at least one or every keyword, it returns the only tweets that contains every keyword
-    #solution was to use the OR in the keywords string as this is for tweets search only and might give errors in pure python
-    limit = 10
+keywords = ['Buhari OR APC OR  PeterObi OR Tinubu OR PDP OR Atiku OR LabourParty']
+#keywords = ['Buhari','APC', 'PeterObi','Tinubu','Atiku']
+#it seems the api does not return every tweet containing at least one or every keyword, it returns the only tweets that contains every keyword
+#solution was to use the OR in the keywords string as this is for tweets search only and might give errors in pure python
+limit = 10000
 
-    tweets = tweepy.Cursor(api.search_tweets, q = keywords,count = 200, tweet_mode = 'extended',geocode='9.0820,8.6753,450mi', until=week_ago).items(limit)
+tweets = tweepy.Cursor(api.search_tweets, q = keywords,count = 200, tweet_mode = 'extended',geocode='9.0820,8.6753,450mi', until=yester).items(limit)
 
-    columns = ['time_created', 'screen_name','name', 'tweet','loca_tion', 'descrip_tion','verified','followers', 'source','geo_enabled','retweet_count','truncated','lang','likes']
-    data = []
+columns = ['time_created', 'screen_name','name', 'tweet','loca_tion', 'descrip_tion','verified','followers', 'source','geo_enabled','retweet_count','truncated','lang','likes']
+data = []
 
 
-    for tweet in tweets:
-        data.append([tweet.created_at, tweet.user.screen_name, tweet.user.name,tweet.full_text, tweet.user.location, tweet.user.description,tweet.user.verified,tweet.user.followers_count,tweet.source,tweet.user.geo_enabled,tweet.retweet_count,tweet.truncated,tweet.lang,tweet.favorite_count])
-        
-    df = pd.DataFrame(data , columns=columns)
-    df = df[~df.tweet.str.contains("RT")]
-    #removes retweeted tweets
-    df = df.reset_index(drop = True)
-
-    df.to_csv('tweets.csv')
+for tweet in tweets:
+    data.append([tweet.created_at, tweet.user.screen_name, tweet.user.name,tweet.full_text, tweet.user.location, tweet.user.description,tweet.user.verified,tweet.user.followers_count,tweet.source,tweet.user.geo_enabled,tweet.retweet_count,tweet.truncated,tweet.lang,tweet.favorite_count])
     
-    sf = pd.read_csv('tweets.csv')
-    sf = sf.drop(sf.columns[0],axis = 1) #remove unnamed column sf[0]
-    
-    #sf.tweet = sf.tweet.str.replace(r'\W'," ")#replace all non aphabetic characters with space
-    #sf.descrip_tion = sf.descrip_tion.str.replace(r'\W'," ")
-    
-
-    sf.to_csv(r'tweets.csv', index = False, header=True) #save to same csv file
+df = pd.DataFrame(data , columns=columns)
+df = df[~df.tweet.str.contains("RT")]
+#removes retweeted tweets
+df = df.reset_index(drop = True)
+print(df.time_created)
 
 
-def postgre_push():
-    conn = psycopg2.connect(database="test",
-                            user='postgres', password='george9042',
-                            host='127.0.0.1', port='5433'
+conn_string = 'postgresql://myadmin:electionapi@dubem-postgres.carjb4cqbkhg.us-east-2.rds.amazonaws.com:5432/postgres'
+  
+db = create_engine(conn_string)
+conn = db.connect()
+
+df.to_sql('election', con=conn, if_exists='append',
+          index=False)
+conn = psycopg2.connect(database="postgres",
+                            user='myadmin', password='electionapi',
+                            host='dubem-postgres.carjb4cqbkhg.us-east-2.rds.amazonaws.com', port='5432'
     )
+conn.autocommit = True
+cursor = conn.cursor()
+  
+sql1 = '''select * from election;'''
+cursor.execute(sql1)
 
-    conn.autocommit = True
-    cursor = conn.cursor()
+for i in cursor.fetchall():
+    print(i)
+  
 
+conn.close()
 
-    sql = ''' CREATE TABLE IF NOT EXISTS election_tweets
-            (    name varchar(100) NOT NULL,
-                time_created timestamp,
-                screen_name varchar(100),
-                tweet varchar(4000),
-                loca_tion varchar(200),
-                descrip_tion varchar,
-                verified varchar(10) NOT NULL,
-                followers int,
-                source varchar(50),
-                geo_enabled varchar(10) NOT NULL,
-                retweet_count int,
-                truncated varchar(10) NOT NULL,
-                lang varchar(10),
-                likes varchar(10) NOT NULL
-            ) 
-        '''
-
-
-    cursor.execute(sql)
-
-    sql2 = '''COPY election_tweets(time_created,screen_name,name,tweet,loca_tion,descrip_tion,verified,followers,source,geo_enabled,retweet_count,truncated,lang,likes)
-    FROM 'C:/Users/HP/Downloads/Primis/tweets.csv' DELIMITER ',' CSV HEADER;''' 
-
-    cursor.execute(sql2)
-
-   # sql3 = '''select * from election;'''
-    #cursor.execute(sql3)
-    #for i in cursor.fetchall():
-     #   print(i)
-
-    conn.commit()
-    conn.close()
+#2022-10-09 done
+#2022-10-08 done
