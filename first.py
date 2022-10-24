04a3c0c444a2e70047ce69ae8b8a9a39531d2b7e
GeeKaizer <61968966+George-Michael-Dagogo@users.noreply.github.com>
2022-10-24 16:49:21 +0100

    latest

---
+++ first.py
@@ -0,0 +1,77 @@
+import tweepy
+import configparser
+import pandas as pd
+import psycopg2
+from datetime import date
+import datetime 
+import psycopg2
+import pandas as pd
+from sqlalchemy import create_engine
+
+
+today = datetime.date.today()
+week_ago = datetime.date.today() - datetime.timedelta(days=1)
+yester = datetime.date.today() - datetime.timedelta(days=1)
+
+api_key = "your api_key"
+api_key_secret = "your api_key_secret"
+access_token = "your access_token "
+access_token_secret = "your access_token_secret"
+
+auth = tweepy.OAuthHandler(api_key,api_key_secret)
+auth.set_access_token(access_token,access_token_secret)
+
+api = tweepy.API(auth)
+
+keywords = ['Buhari OR APC OR  PeterObi OR Tinubu OR PDP OR Atiku OR LabourParty']
+#keywords = ['Buhari','APC', 'PeterObi','Tinubu','Atiku']
+#it seems the api does not return every tweet containing at least one or every keyword, it returns the only tweets that contains every keyword
+#solution was to use the OR in the keywords string as this is for tweets search only and might give errors in pure python
+limit = 5
+
+tweets = tweepy.Cursor(api.search_tweets, q = keywords,count = 200, tweet_mode = 'extended',geocode='9.0820,8.6753,450mi', until=today).items(limit)
+
+columns = ['time_created', 'screen_name','name', 'tweet','loca_tion', 'descrip_tion','verified','followers', 'source','geo_enabled','retweet_count','truncated','lang','likes']
+data = []
+
+
+for tweet in tweets:
+    data.append([tweet.created_at, tweet.user.screen_name, tweet.user.name,tweet.full_text, tweet.user.location, tweet.user.description,tweet.user.verified,tweet.user.followers_count,tweet.source,tweet.user.geo_enabled,tweet.retweet_count,tweet.truncated,tweet.lang,tweet.favorite_count])
+    
+df = pd.DataFrame(data , columns=columns)
+df = df[~df.tweet.str.contains("RT")]
+#removes retweeted tweets
+df = df.reset_index(drop = True)
+print(df.time_created)
+
+
+conn_string = 'postgresql://user:password@hostname:port/database'
+  
+db = create_engine(conn_string)
+conn = db.connect()
+
+df.to_sql('election', con=conn, if_exists='append',
+          index=False)
+conn = psycopg2.connect(database="database",
+                            user='user', password='password',
+                            host='host', port='port'
+    )
+conn.autocommit = True
+cursor = conn.cursor()
+  
+sql1 = '''SELECT COUNT(*) FROM election;'''
+cursor.execute(sql1)
+
+for i in cursor.fetchall():
+    print(i)
+  
+
+conn.close()
+
+#2022-10-14 done
+#2022-10-13 done
+#2022-10-12 done
+#2022-10-11 done
+#2022-10-10 done
+#2022-10-09 done
+#2022-10-08 done
